8884ee2693ae3ad78bf1fb92983fbdc0ed4bba5e
petrostrak <pit.trak@gmail.com>
2021-11-08 16:06:20 +0200

    Establishing a TCP Connection by Using Go's Standard Library

---
+++ Establishing-a-TCP-Connection/TCP-Server/listen_test.go
@@ -0,0 +1,30 @@
+package tcpserver_test
+
+import (
+	"net"
+	"testing"
+)
+
+func TestListener(t *testing.T) {
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() { _ = listener.Close() }()
+
+	t.Logf("bound to %q", listener.Addr())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		go func(c net.Conn) {
+			defer c.Close()
+		}(conn)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/petrostrak/network-programming-with-go
+
+go 1.17
