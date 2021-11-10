7983ed4ef9ae2c4a23aa710adf4964047416c8c9
petrostrak <pit.trak@gmail.com>
2021-11-10 16:57:42 +0200

    Implementing a Heartbeat

---
+++ 01-Establishing-a-TCP-Connection/08-Implementing-a-Heartbeat/ping.go
@@ -0,0 +1,50 @@
+package implementingaheartbeat
+
+import (
+	"context"
+	"io"
+	"time"
+)
+
+const (
+	defaultPingInterval = 30 * time.Second
+)
+
+func Pinger(ctx context.Context, w io.Writer, reset <-chan time.Duration) {
+	var interval time.Duration
+
+	select {
+	case <-ctx.Done():
+		return
+	case interval = <-reset: // pulled initial interval off reset channel
+	default:
+	}
+
+	timer := time.NewTimer(interval)
+	defer func() {
+		if !timer.Stop() {
+			<-timer.C
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case newInterval := <-reset:
+			if !timer.Stop() {
+				<-timer.C
+			}
+			if newInterval > 0 {
+				interval = newInterval
+			}
+		case <-timer.C:
+			if _, err := w.Write([]byte("ping")); err != nil {
+				// track and act on consecutive timeoutshere
+				return
+			}
+		}
+
+		_ = timer.Reset(interval)
+	}
+}
