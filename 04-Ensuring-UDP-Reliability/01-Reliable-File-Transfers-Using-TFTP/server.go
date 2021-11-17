0ed2805ceef395f7631703cd4890620fe2a9591d
petrostrak <pit.trak@gmail.com>
2021-11-17 16:45:42 +0200

    Server type impl

---
+++ 04-Ensuring-UDP-Reliability/01-Reliable-File-Transfers-Using-TFTP/server.go
@@ -0,0 +1,67 @@
+package reliablefiletransfersusingtftp
+
+import (
+	"errors"
+	"log"
+	"net"
+	"time"
+)
+
+type Server struct {
+	Payload []byte        // the payload served for all read requests
+	Retries uint8         // the number of times to retry a failed transmission
+	Timeout time.Duration // the duration to wait for an acknowledgement
+}
+
+func (s Server) ListenAndServe(addr string) error {
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = conn.Close() }()
+
+	log.Printf("Listening on %s ...\n", conn.LocalAddr())
+
+	return s.Serve(conn)
+}
+
+func (s Server) Serve(conn net.PacketConn) error {
+	if conn == nil {
+		return errors.New("nil connection")
+	}
+
+	if s.Payload == nil {
+		return errors.New("payload required")
+	}
+
+	if s.Retries == 0 {
+		s.Retries = 10
+	}
+
+	if s.Timeout == 0 {
+		s.Timeout = 6 * time.Second
+	}
+
+	var rrq ReadReq
+
+	for {
+		buf := make([]byte, DatagramSize)
+
+		_, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			return err
+		}
+
+		err = rrq.UnmarshalBinary(buf)
+		if err != nil {
+			log.Printf("[%s] bad request: %v", addr, err)
+			continue
+		}
+
+		go s.handle(addr.String(), rrq)
+	}
+}
+
+func (s Server) handle(clientAddr string, rrq ReadReq) {
+
+}
