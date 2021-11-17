732cf726c1c02887e933ab8f7d50e5a879cca365
petrostrak <pit.trak@gmail.com>
2021-11-17 16:21:58 +0200

    Reliable file transfers using TFTP - Acknowledgement type impl

--- 04-Ensuring-UDP-Reliability/01-Reliable-File-Transfers-Using-TFTP/types.go
+++ 04-Ensuring-UDP-Reliability/01-Reliable-File-Transfers-Using-TFTP/types.go
@@ -197,3 +197,41 @@ func (d *Data) UnmarshalBinary(p []byte) error {
 
 	return nil
 }
+
+type Ack uint16
+
+func (a Ack) MarshalBinary() ([]byte, error) {
+	cap := 2 + 2 // operation code + block number
+
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpAck) // write operation code
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(b, binary.BigEndian, a) // write block number
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (a *Ack) UnmarshalBinary(p []byte) error {
+	var code OpCode
+
+	r := bytes.NewReader(p)
+
+	err := binary.Read(r, binary.BigEndian, &code) // read operation code
+	if err != nil {
+		return err
+	}
+
+	if code != OpAck {
+		return errors.New("invalid ACK")
+	}
+
+	return binary.Read(r, binary.BigEndian, a) // read block number
+}
