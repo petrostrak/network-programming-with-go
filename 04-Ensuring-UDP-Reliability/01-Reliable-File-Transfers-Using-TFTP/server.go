c1b63d049f93c6cd3fe3961a5927a3d8c924ef36
petrostrak <pit.trak@gmail.com>
2021-11-18 11:01:17 +0200

    Handling read requests

--- 04-Ensuring-UDP-Reliability/01-Reliable-File-Transfers-Using-TFTP/server.go
+++ 04-Ensuring-UDP-Reliability/01-Reliable-File-Transfers-Using-TFTP/server.go
@@ -1,6 +1,7 @@
 package reliablefiletransfersusingtftp
 
 import (
+	"bytes"
 	"errors"
 	"log"
 	"net"
@@ -63,5 +64,68 @@ func (s Server) Serve(conn net.PacketConn) error {
 }
 
 func (s Server) handle(clientAddr string, rrq ReadReq) {
+	log.Printf("[%s] requested file: %s", clientAddr, rrq.Filename)
 
+	conn, err := net.Dial("udp", clientAddr)
+	if err != nil {
+		log.Printf("[%s] dial: %v", clientAddr, err)
+		return
+	}
+	defer func() { _ = conn.Close() }()
+
+	var (
+		ackPkt  Ack
+		errPkt  Err
+		dataPkt = Data{Payload: bytes.NewReader(s.Payload)}
+		buf     = make([]byte, DatagramSize)
+	)
+
+NEXTPACKET:
+	for n := DatagramSize; n == DatagramSize; {
+		data, err := dataPkt.MarshalBinary()
+		if err != nil {
+			log.Printf("[%s] preparing data packet: %v", clientAddr, err)
+			return
+		}
+
+	RETRY:
+		for i := s.Retries; i > 0; i-- {
+			n, err = conn.Write(data) // send the data packet
+			if err != nil {
+				log.Printf("[%s] write: %v", clientAddr, err)
+				return
+			}
+
+			// wait for the client's ACK packet
+			_ = conn.SetReadDeadline(time.Now().Add(s.Timeout))
+
+			_, err = conn.Read(buf)
+			if err != nil {
+				if nErr, ok := err.(net.Error); ok && nErr.Timeout() {
+					continue RETRY
+				}
+
+				log.Printf("[%s] waiting for ACK: %v", clientAddr, err)
+				return
+			}
+
+			switch {
+			case ackPkt.UnmarshalBinary(buf) == nil:
+				if uint16(ackPkt) == dataPkt.Block {
+					// received ACK; send necxt data packet
+					continue NEXTPACKET
+				}
+			case errPkt.UnmarshalBinary(buf) == nil:
+				log.Printf("[%s] received error: %v", clientAddr, errPkt.Message)
+				return
+			default:
+				log.Printf("[%s] bad packet", clientAddr)
+			}
+		}
+
+		log.Printf("[%s] exhausted retries", clientAddr)
+		return
+	}
+
+	log.Printf("[%s] sent %d blocks", clientAddr, dataPkt.Block)
 }
