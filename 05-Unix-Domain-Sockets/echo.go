5976f11f5a6b441ad041860cc1091d2f64dc00bb
petrostrak <pit.trak@gmail.com>
2021-11-18 16:47:26 +0200

    Unix domain sockets - Echo server

---
+++ 05-Unix-Domain-Sockets/echo.go
@@ -0,0 +1,55 @@
+package unixdomainsockets
+
+import (
+	"context"
+	"net"
+)
+
+func streamingEchoServer(ctx context.Context, network, addr string) (net.Addr, error) {
+	s, err := net.Listen(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+
+		// Once the caller cancels the context, the server closes.
+		go func() {
+			<-ctx.Done()
+			_ = s.Close()
+		}()
+
+		for {
+
+			// Once the server accepts a connection, we start a goroutine to
+			// echo incoming messages.
+			conn, err := s.Accept()
+			if err != nil {
+				return
+			}
+
+			go func() {
+				defer func() { _ = conn.Close() }()
+
+				// Since we are using the net.Conn interface, we can use the
+				// Read and Write methods to communicate with the client no
+				// matter whether the server is communicating over a network
+				// socket or a Unix domain socket.
+				for {
+					buf := make([]byte, 1024)
+					n, err := conn.Read(buf)
+					if err != nil {
+						return
+					}
+
+					_, err = conn.Write(buf[:n])
+					if err != nil {
+						return
+					}
+				}
+			}()
+		}
+	}()
+
+	return s.Addr(), nil
+}
