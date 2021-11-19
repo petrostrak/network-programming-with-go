8f59304396c557d484cfaae86e301916c9445299
petrostrak <pit.trak@gmail.com>
2021-11-19 20:02:15 +0200

    The unixgram Datagram socket

--- 05-Unix-Domain-Sockets/echo.go
+++ 05-Unix-Domain-Sockets/echo.go
@@ -3,6 +3,7 @@ package unixdomainsockets
 import (
 	"context"
 	"net"
+	"os"
 )
 
 func streamingEchoServer(ctx context.Context, network, addr string) (net.Addr, error) {
@@ -52,4 +53,36 @@ func streamingEchoServer(ctx context.Context, network, addr string) (net.Addr, e
 	}()
 
 	return s.Addr(), nil
+}
+
+func datagramEchoServer(ctx context.Context, network, addr string) (net.Addr, error) {
+	s, err := net.ListenPacket(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		go func() {
+			<-ctx.Done()
+			_ = s.Close()
+			if network == "unixgram" {
+				_ = os.Remove(addr)
+			}
+		}()
+
+		buf := make([]byte, 1024)
+		for {
+			n, clientAddr, err := s.ReadFrom(buf)
+			if err != nil {
+				return
+			}
+
+			_, err = s.WriteTo(buf[:n], clientAddr)
+			if err != nil {
+				return
+			}
+		}
+	}()
+
+	return s.LocalAddr(), nil
 }
