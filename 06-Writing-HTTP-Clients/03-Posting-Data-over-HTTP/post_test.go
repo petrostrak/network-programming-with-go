5fd42aa28c8decd4ebc3a086a5f221f88d61d809
petrostrak <pit.trak@gmail.com>
2021-11-20 10:55:13 +0200

    Posting Multipart Form with Attached Files

--- 06-Writing-HTTP-Clients/03-Posting-Data-over-HTTP/post_test.go
+++ 06-Writing-HTTP-Clients/03-Posting-Data-over-HTTP/post_test.go
@@ -2,11 +2,18 @@ package postingdataoverhttp
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 )
 
 type User struct {
@@ -68,4 +75,76 @@ func TestPostUser(t *testing.T) {
 	}
 
 	_ = resp.Body.Close()
+}
+
+// Posting Multipart Form with Attached Files
+func TestMultipartPost(t *testing.T) {
+
+	// Create a new buffer to act as the request body.
+	reqBody := new(bytes.Buffer)
+
+	// Create a multipart writer that wraps the buffer.
+	w := multipart.NewWriter(reqBody)
+
+	for k, v := range map[string]string{
+		"date":        time.Now().Format(time.RFC3339),
+		"description": "Form values with attached files",
+	} {
+		err := w.WriteField(k, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i, file := range []string{
+		"./files/hello.txt",
+		"./files/goodbye.txt",
+	} {
+		filePart, err := w.CreateFormFile(fmt.Sprintf("file%d", i+1), filepath.Base(file))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		f, err := os.Open(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = io.Copy(filePart, f)
+		_ = f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://httpbin.org/post", reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status %d; actual status %d", http.StatusOK, resp.StatusCode)
+	}
+
+	t.Logf("\n%s", b)
 }
