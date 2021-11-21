924ce5073e70c50b4fd87572bd8191bf0374cb63
petrostrak <pit.trak@gmail.com>
2021-11-21 20:42:50 +0200

    The anatomy of a Go HTTP server

--- 07-Building-HTTP-Services/01-The-anatomy-of-a-Go-HTTP-server/server_test.go
+++ 07-Building-HTTP-Services/01-The-anatomy-of-a-Go-HTTP-server/server_test.go
@@ -1,6 +1,10 @@
 package theanatomyofagohttpserver
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"testing"
@@ -29,4 +33,51 @@ func TestSimpleHTTPServer(t *testing.T) {
 			t.Error(err)
 		}
 	}()
+
+	testCases := []struct {
+		method   string
+		body     io.Reader
+		code     int
+		response string
+	}{
+		{http.MethodGet, nil, http.StatusOK, "Hello, friend"},
+		{http.MethodPost, bytes.NewBufferString("<world>"), http.StatusOK, "Hello, &lt;world&gt;!"},
+		{http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+	}
+
+	client := new(http.Client)
+	path := fmt.Sprintf("http://%s/", srv.Addr)
+
+	for i, c := range testCases {
+		r, err := http.NewRequest(c.method, path, c.body)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+
+		resp, err := client.Do(r)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+
+		if resp.StatusCode != c.code {
+			t.Errorf("%d: unexpected status code: %q", i, resp.Status)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+		_ = resp.Body.Close()
+
+		if c.response != string(b) {
+			t.Errorf("%d: expected %q; actual %q", i, c.response, b)
+		}
+	}
+
+	if err := srv.Close(); err != nil {
+		t.Fatal(err)
+	}
 }
