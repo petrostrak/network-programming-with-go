404f64af0d2d251aed8c4b73013487c26237d319
petrostrak <pit.trak@gmail.com>
2021-11-23 15:14:50 +0200

    Test Restrict Prefix method

--- 07-Building-HTTP-Services/02-Middleware/restrict_prefix.go
+++ 07-Building-HTTP-Services/02-Middleware/restrict_prefix.go
@@ -2,8 +2,10 @@ package middleware
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"path"
 	"strings"
+	"testing"
 )
 
 func RestrictPrefix(prefix string, next http.Handler) http.Handler {
@@ -19,4 +21,28 @@ func RestrictPrefix(prefix string, next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		},
 	)
+}
+
+func TestRestrictPrefix(t *testing.T) {
+	handler := http.StripPrefix("/static/", RestrictPrefix(".", http.FileServer(http.Dir("../files/"))))
+
+	testCases := []struct {
+		path string
+		code int
+	}{
+		{"http://test/static/sage.svg", http.StatusOK},
+		{"http://test/static/.secret", http.StatusNotFound},
+		{"http://test/static/.dir/secret", http.StatusNotFound},
+	}
+
+	for i, c := range testCases {
+		r := httptest.NewRequest(http.MethodGet, c.path, nil)
+		w := httptest.NewRecorder()
+		handler.ServeHTTP(w, r)
+
+		actual := w.Result().StatusCode
+		if c.code != actual {
+			t.Fatalf("%d: expected %d; actual %d", i, c.code, actual)
+		}
+	}
 }
