13f1be01ab846adf35673b08f3e86f000f48b513
petrostrak <pit.trak@gmail.com>
2021-11-24 20:50:21 +0200

    Deserializing chores from file, flushing to storag and printing the list

--- 09-Data-Serialization/cmd/housework.go
+++ 09-Data-Serialization/cmd/housework.go
@@ -1,5 +1,89 @@
 package cmd
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/petrostrak/network-programming-with-go/09-Data-Serialization/housework"
+	storage "github.com/petrostrak/network-programming-with-go/09-Data-Serialization/json"
+)
+
+var (
+	datafile string
+)
+
+func init() {
+	flag.StringVar(&datafile, " file", "housework.db", "data file")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			`Usage: %s [flags] [add chore, ...|complete #]
+	add      add comma-separater chores
+	complete complete designated chore
+	
+Flags:
+	`, filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+func load() ([]*housework.Chore, error) {
+	if _, err := os.Stat(datafile); os.IsNotExist(err) {
+		return make([]*housework.Chore, 0), nil
+	}
+
+	df, err := os.Open(datafile)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := df.Close(); err != nil {
+			fmt.Printf("closing data file: %v", err)
+		}
+	}()
+
+	return storage.Load(df)
+}
+
+func Flush(chores []*housework.Chore) error {
+	df, err := os.Create(datafile)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := df.Close(); err != nil {
+			fmt.Printf("closing data file: %v", err)
+		}
+	}()
+
+	return storage.Flush(df, chores)
+}
+
+func list() error {
+	chores, err := load()
+	if err != nil {
+		return err
+	}
+
+	if len(chores) == 0 {
+		fmt.Println("You're all caught up!")
+		return nil
+	}
+
+	fmt.Println("#\t[X]\tDescription")
+	for i, chore := range chores {
+		c := " "
+		if chore.Complete {
+			c = "X"
+		}
+		fmt.Printf("%d\t[%s]\t%s\n", i+1, c, chore.Description)
+	}
+
+	return nil
+}
+
 func main() {
 
 }
