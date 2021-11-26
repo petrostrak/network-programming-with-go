0c6a2c8d5b48047016ff7d44fcd03241842be738
petrostrak <pit.trak@gmail.com>
2021-11-27 00:01:05 +0200

    Protobuf storage implementation

--- 09-Data-Serialization/protobuf/housework.go
+++ 09-Data-Serialization/protobuf/housework.go
@@ -1,1 +1,34 @@
 package protobuf
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/petrostrak/network-programming-with-go/09-Data-Serialization/housework/v1"
+)
+
+func Load(r io.Reader) ([]*housework.Chore, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var chores housework.Chores
+
+	return chores.Chores, proto.Unmarshal(b, &chores)
+}
+
+func Flush(w io.Writer, chores []*housework.Chore) error {
+	b, err := proto.Marshal(&housework.Chores{Chores: chores})
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
