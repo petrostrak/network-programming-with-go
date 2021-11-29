4e71733cf6e7edf721df283870f502ed34d61f7d
petrostrak <pit.trak@gmail.com>
2021-11-29 18:59:02 +0200

    Logging simultaneously to a log file and standard output

--- 10-Logging-and-Metrics/01-The-log-package/log_test.go
+++ 10-Logging-and-Metrics/01-The-log-package/log_test.go
@@ -1,6 +1,8 @@
 package thelogpackage
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 )
@@ -8,4 +10,16 @@ import (
 func Example_log() {
 	l := log.New(os.Stdout, "example:", log.Lshortfile)
 	l.Print("logging to standard output")
+}
+
+func Example_logMultiWriter() {
+	logFile := new(bytes.Buffer)
+
+	w := SustainedMultiWriter(os.Stdout, logFile)
+	l := log.New(w, "example:", log.Lshortfile|log.Lmsgprefix)
+
+	fmt.Println("standard output:")
+	l.Print("This is Peter")
+
+	fmt.Print("\nlog file contents:\n", logFile.String())
 }
